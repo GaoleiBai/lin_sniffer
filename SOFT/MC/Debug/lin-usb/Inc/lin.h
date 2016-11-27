073f85af5fc64d3b622cb4b71e7d437c74146528
AlexNL <alexanderliakhovets@gmail.com>
2016-11-27 21:48:06 +0300

    Refactor all files

--- SOFT/MC/Debug/lin-usb/Inc/lin.h
+++ SOFT/MC/Debug/lin-usb/Inc/lin.h
@@ -8,14 +8,14 @@
 #define  LIN_MAX_DATA                   8  // max 8 Datenytes
 #define  LIN_RX_TX_ARRAY_LENGHT        12  // Array for tx/rx lin frane to/from PC 
 
-#define  LIN_POWERON_DELAY          10  // Pause after PowerOn     (ca. 10ms)
-#define  LIN_AKTIV_DELAY             1  // Pause for Transceiver (ca. 50us)
+#define  LIN_POWERON_DELAY             10  // Pause after PowerOn     (ca. 10ms)
+#define  LIN_AKTIV_DELAY                1  // Pause for Transceiver (ca. 50us)
 
-#define  LIN_INTER_FRAME_DELAY      10  // Pause (Frame->Frame)   (ca. 10ms)
-#define  LIN_FRAME_RESPONSE_DELAY    2  // Pause (Header->Data)   (ca.  2ms)
-#define  LIN_BREAKFIELD_DELAY        4  // Pause (Breakfield)     (ca.  4ms)
-#define  LIN_DATA_BYTE_DELAY         1  // Pause (Data->Data)     (ca.  1ms)
-#define  LIN_RX_TIMEOUT_CNT          5  // timeout when recive (ca.  5ms)
+#define  LIN_INTER_FRAME_DELAY         10  // Pause (Frame->Frame)   (ca. 10ms)
+#define  LIN_FRAME_RESPONSE_DELAY       2  // Pause (Header->Data)   (ca.  2ms)
+#define  LIN_BREAKFIELD_DELAY           4  // Pause (Breakfield)     (ca.  4ms)
+#define  LIN_DATA_BYTE_DELAY            1  // Pause (Data->Data)     (ca.  1ms)
+#define  LIN_RX_TIMEOUT_CNT             5  // timeout when recive (ca.  5ms)
 
 #define HAL_LIN_UART_TIMEOUT          100  // Timeout for HAL_Uart_Transmit function 
 
@@ -26,6 +26,7 @@ extern UART_HandleTypeDef huart1;
 
 extern uint8_t linRxBff[1];
 extern uint8_t farmeBuffPtr;
+
 //
 // LIN farme data structure
 //
@@ -57,6 +58,7 @@ typedef enum {
 	WAIT_CHECKSUM
 }LIN_RXMODE_t;
 
+
 typedef struct {
   LIN_RXMODE_t mode;  // currebt Mode
   uint8_t data_ptr; // Datenpointer  
@@ -70,6 +72,7 @@ typedef enum {
   LIN_RX_EMPTY,  //no Frame recived
   LIN_WRONG_CRC  // Wrong CRC
 }LIN_ERR_t;
+
 
 void UB_LIN_Master_Init(void);
 void AddFrameToBuff(LIN_FRAME_t lin_frame);
