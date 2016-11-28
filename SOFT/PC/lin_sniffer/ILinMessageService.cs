397c5c863da3d774608dd3f70bfa9656993d6c1b
AlexNL <alexanderliakhovets@gmail.com>
2016-11-28 20:17:02 +0300

    Create LIN static class. Create ILinMessageService, LinMessageService.

---
+++ SOFT/PC/lin_sniffer/ILinMessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lin_sniffer
+{
+	public interface ILinMessageService
+	{
+		void PrintIncomintMessage(List<byte> messageArray);
+
+	}
+}
