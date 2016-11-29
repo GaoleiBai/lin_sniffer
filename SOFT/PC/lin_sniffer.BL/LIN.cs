d3deec9f68cb6e5142ecff878a1c4e100b911a7e
AlexNL <alexanderliakhovets@gmail.com>
2016-11-29 22:53:02 +0300

    add property and custom constructor

--- SOFT/PC/lin_sniffer.BL/LIN.cs
+++ SOFT/PC/lin_sniffer.BL/LIN.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace lin_sniffer.BL
 {
-	public static class LIN
+	public class LIN
 	{
-		public const int IncomingArrayLenght = 12; 
+		public const int IncomingArrayLenght = 12;
+
+		byte Header { get; set; }
+		byte ID { get; set; }
+		byte DataLenght { get; set; }
+		byte[] Data { get; set; }
+		byte CRC { get; set; }
+
+		public LIN(byte header, byte id, byte dataLenght, byte[] data, byte crc)
+		{
+			Header = header;
+			ID = id;
+			DataLenght = dataLenght;
+			Data = data;
+			CRC = crc;
+		}
 	}
 }
